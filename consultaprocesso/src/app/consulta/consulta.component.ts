6f2ba938563e1cff892f4333f4029024ecc2915c
Flavio Camello <camellosi@hotmail.com>
2019-10-25 08:33:20 -0300

    Tentando chamar api sigmine
    Nada

--- consultaprocesso/src/app/consulta/consulta.component.ts
+++ consultaprocesso/src/app/consulta/consulta.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProcessoService } from '../processo.service';
 
 @Component({
   selector: 'app-consulta',
   templateUrl: './consulta.component.html',
-  styleUrls: ['./consulta.component.css']
+  styleUrls: ['./consulta.component.css'],
+  providers: [ ProcessoService ]
 })
 export class ConsultaComponent implements OnInit {
 
   public numeroProcesso: string
-  constructor() { }
+  public consulta: Observable<any>
+  processoService: ProcessoService
+  constructor(processoService: ProcessoService) { 
+    this.processoService = processoService
+  }
 
-  ngOnInit() {
+  ngOnInit() { 
+    
   }
 
   public consultarProcesso(): void{
       console.log("O numero do processo é: ",this.numeroProcesso)
+
+      this.consulta = this.processoService.retornaDadosProcesso();
+
+      this.consulta.subscribe({
+        next(retorno) { console.log('Retorno: ', retorno); },
+        error(msg) { console.log('Error Getting Location: ', msg); }
+      });
+
+      
+
+      
   }
 }
--- consultaprocesso/src/app/processo.service.ts
+++ consultaprocesso/src/app/processo.service.ts
@@ -1,5 +1,24 @@
-class ProcessoService{
+import { Observable } from 'rxjs'
+import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { map } from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ProcessoService{
+    url: string = "https://localhost:44363/api/values"
+
+    Hheaders: HttpHeaders = new HttpHeaders()
+      .set('content-type', 'application/json')
+      .set('Accept', 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3');
+
+constructor(private http: HttpClient){
 
 }
 
-export default ProcessoService
+    public retornaDadosProcesso(): Observable<any>{
+        return this.http.get(this.url, {headers: this.Hheaders})
+    }
+}
+
+//https://aluracar.herokuapp.com/salvarpedido?carro=Astra+Sedan&email=asdasd&endereco=Asd&nome=TIago&preco=40300
+//https://sigmine.dnpm.gov.br/arcgis/rest/services/extra/dados_dnpm/MapServer/0/query?text=896173%2F2018&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&where=&returnGeometry=false&outSR=4326&outFields=NOME%2CAREA_HA%2CFASE%2CULT_EVENTO%2CShape&f=pjson
